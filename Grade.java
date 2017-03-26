8829b3d30f296ba64400a659b8a1974d7c2ccec4
Hongjun Chen <hongjunchen@192-168-1-4.tpgi.com.au>
2017-03-26 16:26:32 +1100

    first commit

---
+++ Grade.java
@@ -0,0 +1,24 @@
+public class Grade{
+    public static void main(String[] args){
+        String grade = args[0];
+        switch(grade){
+            case "H":
+                System.out.println("[85, 100]");
+                break;
+            case "D":
+                System.out.println("[75, 84]");
+                break;
+            case "C":
+                System.out.println("[65, 74]");
+                break;
+            case "E":
+                System.out.println("[50, 64]");
+                break;
+            case "F":
+                System.out.println("[0, 49]");
+                break;
+            default:
+                System.out.println("You did not enter a valid value!");
+        }
+    }
+}
