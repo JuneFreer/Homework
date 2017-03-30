c73b20914f352bd0b8a05bc0555c0a591ae041ed
JuneFreer <chjun2016@gmail.com>
2017-03-30 12:28:30 +1100

    error change

--- Remainder.java
+++ Remainder.java
@@ -3,7 +3,7 @@ public class Remainder
     public static void main(String[] args)
     {
         String[] week = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
-        int remainder = 30 % 7;
+        int remainder = 10 % 7;
         String dueDay = week[2 + remainder];
         System.out.println(dueDay);
     }
